554ddf0bbd03af4f21e256a2323f17e91d70a082
Chloe Girodet <cgi@obvious.tech>
2023-02-08 11:53:13 +0100

    EC-16 Create User & Login

--- server/src/entity/users.ts
+++ server/src/entity/users.ts
@@ -1,5 +1,7 @@
-import { Field, ObjectType } from 'type-graphql';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsEmail, MinLength } from 'class-validator';
+import { Field, InputType, ObjectType } from 'type-graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import argon2, { hash, verify } from 'argon2';
 
 @Entity()
 @ObjectType()
@@ -9,7 +11,35 @@ class User {
   id: number;
 
   @Field()
-  name: string;
+  @Column({unique: true})
+  email: string
+
+  @Column()
+  hashedPassword: string
+}
+
+@InputType()
+export class UserInput {
+  @Field()
+  @IsEmail()
+  email: string
+
+  @Field()
+  @MinLength(8)
+  password: string
+}
+
+const hashingOptions = {
+  type: argon2.argon2id,
+  memoryCost: 2 ** 16,
+}
+
+export async function hashPassword(plain: string): Promise<string> {
+  return await hash(plain, hashingOptions);
+}
+
+export async function verifyPassword(plain: string, hashed: string): Promise<boolean> {
+  return await verify(hashed, plain, hashingOptions);
 }
 
 export default User;
--- server/src/resolvers/resolver.ts
+++ server/src/resolvers/resolver.ts
@@ -1,12 +1,27 @@
-import User from '../entity/users';
-import db from '../db';
-import { Query, Resolver } from 'type-graphql';
+import User, { hashPassword, UserInput, verifyPassword } from '../entity/users';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import datasource from '../db';
+import { ApolloError } from 'apollo-server-errors';
 
-@Resolver(User)
+@Resolver(() => User)
 export class UserResolver {
+  @Mutation(() => User)
+  async createUser(@Arg("data"){email, password}: UserInput): Promise<User> {
+    const hashedPassword = await hashPassword(password)
+    return await datasource.getRepository(User).save({email, hashedPassword})
+  }
+
+  @Mutation(() => Boolean)
+  async login(@Arg("data"){email, password}: UserInput): Promise<boolean> {
+    const user = await datasource.getRepository(User).findOneBy({email})
+    if (user === null || !(await verifyPassword(password, user.hashedPassword)))
+    throw new ApolloError("Invalid Credentials", "INVALID_CREDS")
+    return true;
+  }
+
   @Query(() => [User])
-  async usersList(): Promise<User[]> {
-    const users: User[] = await db.getRepository(User).find();
+  async users(): Promise<User[]> {
+    const users: User[] = await datasource.getRepository(User).find();
     return users;
   }
 }
