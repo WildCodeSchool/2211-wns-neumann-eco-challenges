7908a86faaaf8a3ebbf61a352556cf92274f3f53
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2023-04-04 16:49:35 +0200

    Update app structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,20 +4,25 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import "./App.scss";
 import { Dashboard } from "./screens/Dashboard";
+import Grid from "@mui/material/Grid";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <main>
+    <Grid container className="App-header">
+      <main>
+        <Grid>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
           </Routes>
-        </main>
-      </header>
-    </div>
+        </Grid>
+      </main>
+    </Grid>
   );
 }
 
