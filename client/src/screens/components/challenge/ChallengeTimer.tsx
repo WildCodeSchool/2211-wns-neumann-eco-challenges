5136d8dfd9699764d444e4acbc28313adbd988f2
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2023-04-04 16:44:50 +0200

    EC-102 Create ChallengeTimer component

---
+++ client/src/screens/components/challenge/ChallengeTimer.tsx
@@ -0,0 +1,81 @@
+import moment, { Moment } from "moment";
+import momentTZ from "moment-timezone";
+import { useEffect, useState } from "react";
+import { ChallengeTimerProps } from "../../../interfaces/challenge/challenge.interface";
+
+export const ChallengeTimer = ({
+  startingDateTime,
+  endingDateTime,
+  format,
+  type,
+}: ChallengeTimerProps & {
+  format: "graphic" | "text";
+  type: "remaining" | "elapsed" | "untilBeginning";
+}) => {
+  const formatDateValueElement = (value: number, suffix: string) => {
+    const stringValue = value < 10 ? `0${value}` : value.toString();
+    return stringValue + suffix;
+  };
+
+  const formatDate = (startingDateTime: Moment) => {
+    let durationMs = momentTZ(startingDateTime).diff(
+      momentTZ().utc(),
+      "seconds"
+    );
+    // Higher than a month : display full date
+    const dayMs = 60 * 60 * 24;
+    const days = Math.floor(durationMs / dayMs);
+    if (days > 30) {
+      return startingDateTime.format("YY/MM/DD HH:mm:ss");
+    } else {
+      durationMs -= days * dayMs;
+      const hours = Math.floor(durationMs / (dayMs / 24));
+      durationMs -= hours * (dayMs / 24);
+      const minutes = Math.floor(durationMs / (dayMs / 24 / 60));
+      durationMs -= minutes * (dayMs / 24 / 60);
+
+      const formatedDate = [
+        formatDateValueElement(days, "d"),
+        formatDateValueElement(hours, "h"),
+        formatDateValueElement(minutes, "m"),
+        formatDateValueElement(durationMs, "s"),
+      ];
+
+      let startIndex = formatedDate.findIndex((value) => !value.includes("00"));
+      if (startIndex === -1) return "Undefined date";
+      if (startIndex === formatedDate.length - 1) startIndex -= 1;
+      return formatedDate.slice(startIndex, startIndex + 2).join("");
+    }
+  };
+
+  const [displayedTime, setDisplayedTime] = useState("");
+  useEffect(() => {
+    const elapsedTime = moment().diff(moment(startingDateTime));
+    const durationTime = moment(endingDateTime).diff(moment(startingDateTime));
+    const remainingTime = moment(endingDateTime).diff(moment());
+
+    const timeToDisplay = (
+      type === "remaining"
+        ? remainingTime
+        : type === "elapsed"
+        ? format === "text"
+          ? elapsedTime / durationTime
+          : (elapsedTime / durationTime) * 100
+        : formatDate(startingDateTime)
+    ).toString();
+
+    setTimeout(() => setDisplayedTime(timeToDisplay), 1000);
+  }, [displayedTime]);
+
+  if (format === "graphic")
+    return (
+      <div
+        className="challengeTimer"
+        style={{
+          backgroundImage: `conic-gradient(rgba(255,255,255,0.5) ${displayedTime}%, rgba(0,0,0,0) 17%)`,
+        }}
+      ></div>
+    );
+
+  return <div>{displayedTime}</div>;
+};
