4bf413fb6e4de17db318e1e867b8fa6b625c10b6
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2023-04-04 16:47:50 +0200

    EC-104 Create notification center button

--- client/src/screens/components/HeaderScreen.tsx
+++ client/src/screens/components/HeaderScreen.tsx
@@ -1,5 +1,5 @@
-import { Stack, Typography } from "@mui/material";
-
+import { Grid, IconButton, Stack, Typography } from "@mui/material";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 export const HeaderScreen = ({
   title,
   subtitle,
@@ -8,17 +8,37 @@ export const HeaderScreen = ({
   subtitle: string;
 }) => {
   return (
-    <Stack spacing={-1}>
-      <Typography className="headerTitle" fontWeight={800} variant="h5">
-        {title}
-      </Typography>
-      <Typography
-        className="headerSubtitle"
-        fontWeight={700}
-        variant="subtitle1"
+    <Grid container direction={"row"}>
+      <Grid item xs={10}>
+        <Typography className="headerTitle" fontWeight={800} variant="h4">
+          {title}
+        </Typography>
+        <Typography
+          className="headerSubtitle"
+          fontWeight={700}
+          variant="h6"
+          lineHeight={0.5}
+        >
+          {subtitle}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        xs={2}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
       >
-        {subtitle}
-      </Typography>
-    </Stack>
+        <IconButton style={{ border: "2px solid rgba(16,68,85,0.23)" }}>
+          <div className="notificationCircle"></div>
+
+          <NotificationsNoneIcon
+            style={{}}
+            fontSize="medium"
+            htmlColor="#114B5E"
+          ></NotificationsNoneIcon>
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 };
