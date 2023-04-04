6125ffccff1d54d07f97b264917b156abcd942f3
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2023-04-04 16:49:13 +0200

    EC-105 Add more component

--- client/src/screens/components/DashboardWrapper.tsx
+++ client/src/screens/components/DashboardWrapper.tsx
@@ -1,6 +1,6 @@
-import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-
+import { MoreButton } from "./MoreButton";
 export const DashboardWrapper = ({
   title,
   subtitle,
@@ -11,14 +11,29 @@ export const DashboardWrapper = ({
   component: React.ReactNode;
 }) => {
   return (
-    <Stack spacing={0}>
-      <Typography fontWeight={700} variant="h6" lineHeight={1.5}>
-        {title}
-      </Typography>
-      <Typography fontWeight={400} variant="subtitle1">
-        {subtitle}
-      </Typography>
-      {component}
-    </Stack>
+    <Grid rowGap={2}>
+      <Grid container direction={"row"} marginBottom={2}>
+        <Grid item xs={10}>
+          <div>
+            <Typography fontWeight={700} variant="h6" lineHeight={1}>
+              {title}
+            </Typography>
+            <Typography fontWeight={400} variant="subtitle1">
+              {subtitle}
+            </Typography>
+          </div>
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          display="flex"
+          justifyContent={"center"}
+          alignItems="top"
+        >
+          <MoreButton />
+        </Grid>
+      </Grid>
+      <Grid item>{component}</Grid>
+    </Grid>
   );
 };
