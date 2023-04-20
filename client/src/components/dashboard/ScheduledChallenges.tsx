1ea7e59acd4167c5ee2581439cdf07380ad4edb9
Chloe Girodet <cgi@obvious.tech>
2023-04-20 17:30:20 +0200

    update date in Challenges

--- client/src/components/dashboard/ScheduledChallenges.tsx
+++ client/src/components/dashboard/ScheduledChallenges.tsx
@@ -1,7 +1,7 @@
 import Stack from "@mui/material/Stack";
-import momentTZ from "moment-timezone";
 import { ScheduledChallengeItem } from "../challenge/ScheduledChallengeItem";
 import { useChallengesQuery } from "../../gql/generated/schema";
+import moment from "moment";
 
 const attendees = [
   [10, 10],
@@ -11,26 +11,19 @@ const attendees = [
   [0, 13],
 ];
 
-const challengeTime = [
-  [momentTZ().utc().add(1, "day"), momentTZ().utc().add(2, "day")],
-  [momentTZ().utc().add(1, "hour"), momentTZ().utc().add(3, "hour")],
-  [momentTZ().utc().add(47, "minute"), momentTZ().utc().add(56, "minute")],
-  [momentTZ().utc().add(30, "seconds"), momentTZ().utc().add(2, "month")],
-  [momentTZ().utc().add(2, "month"), momentTZ().utc().add(2, "month")],
-];
 export const ScheduledChallenges = () => {
   const { data } = useChallengesQuery();
   const challenges = data?.challenges || [];
   return (
     <Stack spacing={2}>
-      {Array.from(Array(challenges.length).keys()).map((_, index) => (
+      {challenges.map((challenge) => (
         <ScheduledChallengeItem
-          key={index}
-          name={challenges[index]?.name}
-          attendees={attendees[index][0]}
-          endingDateTime={challengeTime[index][1]}
-          startingDateTime={challengeTime[index][0]}
-          expectedAttendees={attendees[index][1]}
+          key={challenge.id}
+          name={challenge.name}
+          attendees={attendees[0][0]}
+          endingDateTime={moment(challenge.endingDate).utc()}
+          startingDateTime={moment(challenge.startingDate).utc()}
+          expectedAttendees={attendees[0][1]}
         />
       ))}
     </Stack>
