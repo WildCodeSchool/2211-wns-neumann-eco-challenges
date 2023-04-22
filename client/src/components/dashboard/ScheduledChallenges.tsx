54b2fb2a3dba0a15d41d18b5b086888d4e2a0b37
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2023-04-22 15:15:00 +0200

    EC-128 Fix challenge timer + filter challenges

--- client/src/components/dashboard/ScheduledChallenges.tsx
+++ client/src/components/dashboard/ScheduledChallenges.tsx
@@ -2,6 +2,7 @@ import Stack from "@mui/material/Stack";
 import { ScheduledChallengeItem } from "../challenge/ScheduledChallengeItem";
 import { useChallengesQuery } from "../../gql/generated/schema";
 import moment from "moment";
+import { getFilteredChallenges } from "../../tools/challenge.tools";
 
 const attendees = [
   [10, 10],
@@ -16,13 +17,13 @@ export const ScheduledChallenges = () => {
   const challenges = data?.challenges || [];
   return (
     <Stack spacing={2}>
-      {challenges.map((challenge) => (
+      {getFilteredChallenges(challenges, "scheduled").map((challenge) => (
         <ScheduledChallengeItem
           key={challenge.id}
           name={challenge.name}
           attendees={attendees[0][0]}
-          endingDateTime={moment(challenge.endingDate).utc()}
-          startingDateTime={moment(challenge.startingDate).utc()}
+          endingDateTime={challenge.endingDate}
+          startingDateTime={challenge.startingDate}
           expectedAttendees={attendees[0][1]}
         />
       ))}
