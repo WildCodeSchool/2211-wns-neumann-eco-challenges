c88d9a6f01854f1af5c3cdd381b1cac8f657cdbf
Chloe Girodet <cgi@obvious.tech>
2023-05-16 15:11:59 +0200

    set mobile app's navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,30 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { ApolloProvider } from "@apollo/client";
 import client from "./gql/client";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ChallengesList from "./components/ChallengesList";
+import Notifications from "./screens/Notifications";
 import UploadPicture from "./components/UploadPicture";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <>
-      <ApolloProvider client={client}>
-        <View style={styles.container}>
-          <ChallengesList />
-          <UploadPicture />
-          <Text>Hello</Text>
-          <StatusBar style="auto" />
-        </View>
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Dashboard') {
+                iconName = focused
+                  ? 'leaf'
+                  : 'leaf-outline';
+              } else if (route.name === 'Notifications') {
+                iconName = focused ? 'notifications' : 'notifications-outline';
+              } else if (route.name === 'Challenge photo') {
+                iconName = focused ? 'camera' : 'camera-outline';
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen
+            name="Dashboard"
+            component={ChallengesList}
+            options={{unmountOnBlur: true}}
+          />
+          <Tab.Screen name="Notifications" component={Notifications} />
+          <Tab.Screen name="Challenge photo" component={UploadPicture} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ mobile/screens/Notifications.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function Notifications() {
+  return (
+    <>
+      <Text>Notifications</Text>
+    </>
+  );
+}
